b28c6d8f4ad0ce39de7cfe291b5994fe66bec22d
codemind07 <codemindtechnology@gmail.com>
2022-10-12 06:15:46 +0530

    JS function
    JS function

---
+++ 03_function_Exp.js
@@ -0,0 +1,21 @@
+var test = function(arg1, arg2){
+    console.log("Hellos");
+    return "abc";
+}
+var result = test();
+console.log(result);
+
+
+var variable_name = 10; // int variable_name = 10;
+variable_name: number = 10;
+
+console.log(variable_name);
+
+var square =function(value){
+    console.log(value * value);
+}
+square(5);
+square(10);
+square(20);
+
+console.log(typeof square);
---
+++ demo.js
@@ -0,0 +1,12 @@
+// This is my first program
+// Now I know the two ways to run program 
+console.log("Hello");
+console.log(' My name is Mohit Sharma  ');
+// info means information
+console.info(" This is info ");
+console.trace(""); 
+// warn means warning
+console.warn("I am not that bad guy"); // warn means warning
+console.error(" Opps failed to execute");
+
+console.log(200 );
